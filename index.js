e28993c1e752491d4173125c994ca3a5fa9de5b5
Dimonchik89 <dima.music1989@gmail.com>
2022-06-23 11:24:25 +0300

    add ejs add new structured files

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 // const filePath = path.join(__dirname, "static", "index.html")
 
+
 // 2-й способо роутинга по страницам
 app.use(express.static(path.resolve(__dirname, "static")))
 app.use(requestTime)
@@ -23,10 +24,27 @@ app.use(logger)
 //     res.sendFile(path.resolve(__dirname, "static", "features.html"))
 // })
 
+// Скачать файл
+// app.get("/download", (req, res) => {
+//     console.log(req.requestTime);
+//     res.download(path.resolve(__dirname, "static", "index.html"))
+// })
 
-app.get("/download", (req, res) => {
-    console.log(req.requestTime);
-    res.download(path.resolve(__dirname, "static", "index.html"))
+
+
+//  EJS
+app.set("view engine", "ejs")
+app.set("views", path.resolve(__dirname, "ejs")) // задаем название папки в которой будут лежать файлы ejs к которым будет обращатся express
+
+// console.log(app.get("view engine"))
+console.log(app.get("views"))
+
+app.get("/", (req, res) => {
+    res.render("index", {title: "Main Page", active: "main"})
+})
+
+app.get("/features", (req, res) => {
+    res.render("features", {title: "Features Page", active: "features"})
 })
 
 app.listen(PORT, () => {
